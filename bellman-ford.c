e4f19fb24d20acd1a41b42ee1be3d77e33bdf36f
yonelyvan <yonelyvan@gmail.com>
2016-08-04 16:36:00 -0500

    Update bellman-ford.c

--- bellman-ford.c
+++ bellman-ford.c
@@ -52,7 +52,8 @@ void set_data( char *fl){
 	fscanf(fin, "%d", &num_data);
 	aristas = num_data;
 	e = 0;
-	for (int i = 0; i < num_data; ++i){
+	int i;
+	for ( i = 0; i < num_data; ++i){
 		fscanf(fin, "%c",&c[0]);
 		fscanf(fin, "%c %d %d %d", &c[0], &a, &b, &p);	
 		//printf("%d %d %d\n", a, b, p);
@@ -79,22 +80,6 @@ void printDist() {
 }
 
 
-void bellman_ford_normal(int s) {
-	int i, j;
-	for (i = 0; i < n; ++i)
-		d[i] = INFINITY;
-	d[s] = 0;
-	int sum;
-	for (i = 0; i < n-1 ; ++i){
-		for (j = 0; j < e; ++j){
-			sum = d[edges[j].a] + edges[j].peso;
-			if ( d[edges[j].b] > sum){
-			 	d[edges[j].b] = sum;
-			}
-		}
-	}
-}
-
 
 
 void bellman_ford_paralelo(int s) {
@@ -119,14 +104,12 @@ void bellman_ford_paralelo(int s) {
 
 int main(int argc, char *argv[]) {
 	/* cargamos aristas del archivo*/
-	set_data("dist.in"); //dist.in s4_2.in s8_2.in s10_2.in s15_2.in
+	set_data("s4_2.in"); //dist.in s4_2.in s8_2.in s10_2.in s15_2.in
 	clock_t start = clock();
  	
  	/*
  	ejecucion de bellman ford distancias minimas desde el nodo 0 a los demas nodos
  	*/
-
- 	//bellman_ford_normal(0); 
 	bellman_ford_paralelo(0);
     
     printf("Tiempo: %f \n", ((double)clock() - start) / CLOCKS_PER_SEC);
@@ -136,47 +119,4 @@ int main(int argc, char *argv[]) {
 }
 
 
-/*
-paralelo
 
-s4
-32  nodos
-128 aristas
-Tiempo: 0.064893
-
-real	0m0.026s
-user	0m0.132s
-sys	0m0.000s
-
-----------------
-s8
-256 nodos
-1024 aristas
-Tiempo: 0.328639
-
-real	0m0.030s
-user	0m0.156s
-sys	0m0.000s
-
-----------------
-s10
-1024 nodos 
-4096 aristas
-Tiempo: 1.099674
-
-real	0m0.177s
-user	0m1.168s
-sys	0m0.008s
-
-----------------
-s15
-32768  nodos
-131072 aristas
-Tiempo: 627.560578 
-
-real	1m23.743s
-user	10m21.664s
-sys	0m1.052s
-
-
-*/
